96ada63e03dc8fe476c620cc0ad7d47022f04d17
Jyothi Ganapathiraju <96363260+Jyothi191022@users.noreply.github.com>
2025-10-29 15:58:13 +0530

    first last equal

---
+++ firstLastEqual.java
@@ -0,0 +1,28 @@
+package com.codegnan.arrays;
+
+public class firstLastEqual {
+	public static boolean firstLast6(int[] arr) {
+		if(arr == null || arr.length == 0) {
+			return false;
+		} else {
+			if(arr[0]==arr[arr.length-1] ) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+	}
+
+
+	public static void main(String[] args) {
+		int[] arr1= {6,2,6};
+		System.out.println(firstLast6(arr1));
+		int[] arr2= {4,5,3};
+		System.out.println(firstLast6(arr2));
+		int[] arr3= {6,1,8,6};
+		System.out.println(firstLast6(arr3));
+		int[] arr4= {};
+		System.out.println(firstLast6(arr4));
+	}
+
+}
